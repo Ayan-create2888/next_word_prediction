f597ef4f6f5d792d5c6e71a280966cfef4a8e3b2
Ayan-create2888 <3513ayaanbaba@gmail.com>
2026-02-08 20:41:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ user_input = st.text_input("Enter a sequence of words:", "to be or")
 if st.button("Predict Next Word"):
     if user_input.strip():
         # Call FastAPI endpoint
-        response = requests.post("http://127.0.0.1:8000/predict", json={"text": user_input})
+        response = requests.post("https://http://127.0.0.1:8000/predict", json={"text": user_input})
         if response.status_code == 200:
             result = response.json()
             predicted_word = result.get("predicted_word", "No prediction")
@@ -17,4 +17,5 @@ if st.button("Predict Next Word"):
         else:
             st.error("Error: Could not get prediction from server.")
     else:
+
         st.warning("Please enter some text.")
