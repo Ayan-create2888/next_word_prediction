aad8be9470e87180fa11c57a6e4c57f844177cd2
Ayan-create2888 <3513ayaanbaba@gmail.com>
2026-02-13 10:15:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,7 @@ user_input = st.text_input("Enter a sequence of words:", "to be or")
 if st.button("Predict Next Word"):
     if user_input.strip():
         # Call FastAPI endpoint
-        response = requests.post(
-    "https://your-fastapi-app.onrender.com/predict",
-    json={"text": user_input}
-)
-
+        response = requests.post("https://ayyan2009-api-netword.hf.space/predict", json={"text": user_input})
         if response.status_code == 200:
             result = response.json()
             predicted_word = result.get("predicted_word", "No prediction")
